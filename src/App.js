de316db1134b4e4729f7010cb721c5fda6428a4c
Naimul Islam <naimulislam920@gmail.com>
2023-10-01 00:24:27 +0600

    Top Banner

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TopBanner from "./components/TopBanner/TopBanner";
+
+function App() {
+  return (
+    <div>
+<TopBanner/>
+    </div>
+  );
+}
+
+export default App;
